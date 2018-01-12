898368d1edf9d70599282f21dcd6cd43edb2e365
Kevin Cox <kevincox@kevincox.ca>
2018-01-12 21:00:26 +0000

    Update deps.

--- src/devices.rs
+++ src/devices.rs
@@ -4,8 +4,8 @@ pub static CHROMECAST: Device = Device {
 	container: &[ContainerFormat::MKV],
 	video: &[VideoFormat::H264, VideoFormat::VP8],
 	audio: &[
-		AudioFormat::FLAC,
 		AudioFormat::AAC,
+		AudioFormat::FLAC,
 		AudioFormat::MP3,
 		AudioFormat::Opus,
 		AudioFormat::Vorbis,
