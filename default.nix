0577756529822f77decc2e2a809c61c0a4ad897d
Kevin Cox <kevincox@kevincox.ca>
2018-07-05 20:35:41 +0100

    Add nix git workaround.

--- default.nix
+++ default.nix
@@ -16,5 +16,29 @@ in rec {
 		FFPROBE_BINARY = "${ffmpeg}/bin/ffprobe";
 		
 		doCheck = false;
+
+		# Work around https://github.com/NixOS/nixpkgs/pull/34034
+		postUnpack = ''
+			eval "$cargoDepsHook"
+			unpackFile "$cargoDeps"
+			cargoDepsCopy=$(stripHash $(basename $cargoDeps))
+			chmod -R +w "$cargoDepsCopy"
+			mkdir -p .cargo
+			cat >.cargo/config <<-EOF
+				[source.crates-io]
+				registry = 'https://github.com/rust-lang/crates.io-index'
+				replace-with = 'vendored-sources'
+
+				[source."https://github.com/kevincox/lru-cache.git"]
+				git = "https://github.com/kevincox/lru-cache.git"
+				branch = "entry-api"
+				replace-with = "vendored-sources"
+
+				[source.vendored-sources]
+				directory = '$(pwd)/$cargoDepsCopy'
+			EOF
+			unset cargoDepsCopy
+			export RUST_LOG=warn
+		'';
 	};
 }
